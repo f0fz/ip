c39dc18df1d03db4c777af4c86ad789fa74b02a3
f0fz <simjunyou99@gmail.com>
2020-08-24 21:02:49 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    // Indents and wraps all of Duke's replies in the --- bars.
+    // Has a single line version...
+    private static void reply(String response) {
+        String bar = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n";
+        System.out.println(bar + " >>> " + response + "\n" + bar);
+    }
+    // and a multiline version as well.
+    private static void reply(String[] responses) {
+        String bar = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n";
+        System.out.println(bar);
+        for (String eachResponse : responses) {
+            System.out.println(" " + eachResponse + "\n");
+        }
+        System.out.println(bar);
+    }
+
+    // Gracefully shuts down Duke.
+    // Doesn't do much now, but I'm guessing there will be file IO next time?
+    private static void stop() {
+        reply("Bye. Hope to see you again soon!");
+    }
+
+    // Main function. Main event loop happens here.
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        Scanner scan = new Scanner(System.in);
+
+        String[] greeting = {"Hello! I'm Duke", "What can I do for you?"};
+        reply(greeting);
+
+        // Main event loop!
+        String userInput;
+        while (true) {
+            userInput = scan.nextLine();
+            if (userInput.toLowerCase().equals("bye")) {
+                stop();
+                break; // Exit condition is here...
+            }
+
+            // and everything else is below here.
+            reply(userInput);
+        }
+
     }
 }
